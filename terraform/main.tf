320bb4e72f42d7a9dbcee4453aecd9e724e38ac2
arnoldrx <tagnearnold2080@gmail.com>
2023-07-25 18:08:44 +0100

    new account

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,12 +2,12 @@ module "sandbox" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail              = "<ACCOUNT EMAIL>"
-    AccountName               = "sandbox-aft"
-    ManagedOrganizationalUnit = "Learn AFT"
-    SSOUserEmail              = "<SSO EMAIL>"
-    SSOUserFirstName          = "Sandbox"
-    SSOUserLastName           = "AFT"
+    AccountEmail              = "kamazaki@gmail.com"
+    AccountName               = "Projectx"
+    ManagedOrganizationalUnit = "AFT"
+    SSOUserEmail              = "koma@outlook.com"
+    SSOUserFirstName          = "Koma"
+    SSOUserLastName           = "jira"
   }
 
   account_tags = {
